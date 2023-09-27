273111e1c500cbafecdc1642769bbfebe7e159dd
Lucaserf <luca.serfilippi97@gmail.com>
2023-09-27 12:34:48 +0000

    drain implementation for log template

--- docker_agent_logger/app/src/training_tokenizer.py
+++ docker_agent_logger/app/src/training_tokenizer.py
@@ -2,19 +2,33 @@ import sys
 sys.dont_write_bytecode = True
 from AI import *
 import os
+import drain3
+from drain3.file_persistence import FilePersistence
 
 os.chdir("./docker_agent_logger/app/")
 
 with open("./data/openstack_normal1.log") as f:
     logs = f.read().split("\n")[:-1]
 
-tokenizer = Tokenizer("./bert-base-cased_saved", max_len=512)
-
-dataset = (i for i in logs)
-
-tokenizer.training_tokenizer(dataset,vocab_size = 32000)
-
-tokenizer.saving_tokenizer("./logs_tokenizer")
+persistence = FilePersistence("./drain/drain3_state.bin")
 
 
+config = drain3.template_miner_config.TemplateMinerConfig()
+config.load("./drain/drain3.ini")
 
+template_miner = drain3.TemplateMiner(persistence,config)
+
+
+
+for log in logs[:10]:
+    result = template_miner.add_log_message(log)
+    print(result)
+
+
+# tokenizer = Tokenizer("./bert-base-cased_saved", max_len=512)
+
+# dataset = (i for i in logs)
+
+# tokenizer.training_tokenizer(dataset,vocab_size = 32000)
+
+# tokenizer.saving_tokenizer("./logs_tokenizer")
