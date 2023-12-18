a0982a4059f0724407151b4b294a241326c0a6f0
Lucaserf <luca.serfilippi97@gmail.com>
2023-12-18 11:15:38 +0000

    log generator BGL

--- docker_app/app/src/periodic_log_generator.py
+++ docker_app/app/src/periodic_log_generator.py
@@ -1,27 +1,40 @@
 import time
-import pandas as pd 
-
+import tensorflow as tf
 
 
 data_folder = "/var/data/"
-#first datanode hdfsv2
-with open(data_folder+"HDFS_v2/node_logs/hadoop-hdfs-datanode-mesos-01.log") as f:
-    logs = f.read().split("\n")[:-1]
 
-done = False
-START = time.time()
-counter_prints = 0
-max_len = len(logs)
+raw_ds = (
+    tf.data.TextLineDataset("persistent_volume/data/BGL/BGL.log")
+    # .filter(lambda x: tf.strings.length(x) < MAN_TRAINING_SEQ_LEN)
+    # .batch(128)
+    # .shuffle(buffer_size=256)
+)
 
-while not done:
+def get_labels(data: tf.Tensor):
+    data = data.decode("utf-8")
+    if data[0] == "-":
+        return (data[2:],False)
+    else:
+        return (data,True)
 
-    with open("/var/log/HDFS{}.log".format(counter_prints),"w") as f:
-            f.write(logs[counter_prints]+"\n")
-    counter_prints +=1
+ds = raw_ds.map(lambda x: tf.numpy_function(func=get_labels,inp=[x],Tout=(tf.string,tf.bool)), num_parallel_calls=tf.data.AUTOTUNE).prefetch(
+    tf.data.AUTOTUNE
+)
+
+val_split = 0.2
+ds_size = 4747963
+
+train_size = int((1-val_split) * ds_size)
+val_size = int(val_split * ds_size)
+
+
+val_ds = ds.skip(train_size).take(val_size)
+
+for i,log in enumerate(val_ds):
+    with open("/var/log/BGL{}.log".format(i),"w") as f:
+        f.write(log[0].numpy().decode("utf-8")+"\n")
     time.sleep(0.1)
-
-    if counter_prints == max_len:
-        done = True
 
 print("the dataset is finished")
 
